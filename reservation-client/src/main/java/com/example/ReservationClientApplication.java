9047ab75e2b920b481bfd2a86e4e5b8f3994ee9e
Darek Kaczyński <kaczynskid@gmail.com>
2017-03-31 10:27:49 +0200

    Added reservations client and DiscoveryClient example

---
+++ reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReservationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner discoveryClientDemo(DiscoveryClient discovery) {
+		return args -> {
+			try {
+				log.info("------------------------------");
+				log.info("DiscoveryClient Example");
+
+				discovery.getInstances("reservationservice").forEach(instance -> {
+					log.info("Reservation service: ");
+					log.info("  ID: {}", instance.getServiceId());
+					log.info("  URI: {}", instance.getUri());
+					log.info("  Meta: {}", instance.getMetadata());
+				});
+
+				log.info("------------------------------");
+			} catch (Exception e) {
+				log.error("DiscoveryClient Example Error!", e);
+			}
+		};
+	}
+}
