91d63dce5bb8e37c4e075ec515e5b2c79c12665e
Darek Kaczyński <kaczynskid@gmail.com>
2017-03-31 15:09:14 +0200

    Added verifier service

---
+++ verifier-service/src/test/resources/contracts/shouldAllowReservationIfOldEnough.groovy
@@ -0,0 +1,26 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method('POST')
+        url('/check')
+        body([
+                name: 'John',
+                age: 22
+        ])
+        headers {
+            header('Content-Type', 'application/json')
+        }
+    }
+    response {
+        status 200
+        body([
+                eligible: true
+        ])
+        headers {
+            header('Content-Type', 'application/json')
+        }
+    }
+}
